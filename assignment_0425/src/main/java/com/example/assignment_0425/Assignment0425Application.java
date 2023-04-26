3e6273cde54eff4bb856e3976b0684e81a9717f8
siwonkim <ktldnjs0811@naver.com>
2023-04-26 21:04:25 +0900

    Initial commit

---
+++ assignment_0425/src/main/java/com/example/assignment_0425/Assignment0425Application.java
@@ -0,0 +1,13 @@
+package com.example.assignment_0425;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment0425Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment0425Application.class, args);
+    }
+
+}
---
+++ assignment_0425/src/test/java/com/example/assignment_0425/Assignment0425ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment_0425;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment0425ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
