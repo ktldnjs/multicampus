38e02050ee159bddecc048a5e78b63662ca4df3a
siwonkim <ktldnjs0811@naver.com>
2023-04-26 21:10:58 +0900

    Initial commit

---
+++ html_0424/src/main/java/com/example/html_0424/Html0424Application.java
@@ -0,0 +1,13 @@
+package com.example.html_0424;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html0424Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Html0424Application.class, args);
+    }
+
+}
---
+++ html_0424/src/test/java/com/example/html_0424/Html0424ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.html_0424;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Html0424ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
